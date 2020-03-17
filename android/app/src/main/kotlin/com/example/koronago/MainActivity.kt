37e297a150e0ca20e1721c970d048fcf34abb84e
Techno-Disaster <nitinnirve@gmail.com>
2020-03-18 02:00:01 +0530

    getting basic response from api done

---
+++ android/app/src/main/kotlin/com/example/koronago/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.koronago
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class KoronaGo extends StatefulWidget {
+  @override
+  _KoronaGoState createState() => _KoronaGoState();
+}
+
+class _KoronaGoState extends State<KoronaGo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
