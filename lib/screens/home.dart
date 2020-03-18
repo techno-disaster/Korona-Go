629f257fc0581922c364718535b20209eaa4318f
matejmecka <matej.plavevski+github@gmail.com>
2020-03-18 15:04:19 +0100

    Country Pickeeer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_country_picker/flutter_country_picker.dart';
 
 class KoronaGo extends StatefulWidget {
   @override
@@ -7,10 +7,19 @@ class KoronaGo extends StatefulWidget {
 }
 
 class _KoronaGoState extends State<KoronaGo> {
+  Country _selected;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: CountryPicker(
+        onChanged: (Country country) {
+            setState(() {
+              _selected = country;
+            });
+          },
+          selectedCountry: _selected,
+      )
     );
   }
 }
