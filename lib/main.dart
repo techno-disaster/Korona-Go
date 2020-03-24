ae949ecfd2a1dadcc5904df4bfdf95315785e853
Techno-Disaster <nitinnirve@gmail.com>
2020-03-25 02:11:24 +0530

    backend? done, UI now :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 import 'package:koronago/screens/home.dart';
 
 // import 'screens/home.dart';
-const String url = "https://thevirustracker.com/free-api?countryTotal=IN";
 void main() {
   runApp(MyApp());
-}
-
-Future getData() async {
-  http.Response response = await http.get(url);
-
-  var data = jsonDecode(response.body);
-
-  print(data["countrydata"]);
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: KoronaGo(),
-      // Scaffold(
-      //   body: Center(
-      //     child: RaisedButton(
-      //       onPressed: () {
-      //         print("getting korona stats");
-      //         getData();
-      //       },
-      //       child: Container(
-      //         color: Colors.red,
-      //         height: 100,
-      //         width: 100,
-      //       ),
-      //     ),
-      //   ),
-      // ),
     );
     // MaterialApp(home: KoronaGo(),
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_country_picker/flutter_country_picker.dart';
 
 class KoronaGo extends StatefulWidget {
@@ -7,33 +10,143 @@ class KoronaGo extends StatefulWidget {
 }
 
 class _KoronaGoState extends State<KoronaGo> {
-  Country _selected;
+  Country selected;
+  Future getData(iso) async {
+    var url = 'https://thevirustracker.com/free-api?countryTotal=$iso';
+    http.Response response = await http.get(url);
+
+    var data = jsonDecode(response.body);
+
+    print(data["countrydata"]);
+    return data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData("US");
+  }
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    double screenwidth = queryData.size.width;
+    double screenheight = queryData.size.height;
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: CountryPicker(
-              onChanged: (Country country) {
-                setState(() {
-                  _selected = country;
-                });
-              },
-              selectedCountry: _selected,
-            ),
-          ),
-          RaisedButton(
-            child: Text("Select Country"),
-            onPressed: () {
-              print(_selected.isoCode); //country code is here
+      backgroundColor: Color.fromRGBO(18, 18, 18, 0.8),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(18, 18, 18, 0.8),
+        elevation: 0,
+        actions: <Widget>[
+          CountryPicker(
+            showName: false,
+            showDialingCode: false,
+            onChanged: (Country country) {
+              setState(() {
+                selected = country;
+                getData(selected.isoCode);
+              });
             },
+            selectedCountry: selected,
+          ),
+        ],
+        title: Text("CovidMate"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.fromLTRB(15, 15, 0, 15),
+                    color: Color.fromRGBO(18, 18, 18, 1),
+                    child: Container(
+                      child: FutureBuilder(
+                        future:
+                            getData(selected == null ? "US" : selected.isoCode),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            return Container(
+                              child: Center(
+                                child: Text(
+                                  snapshot.data["countrydata"][0]["total_cases"]
+                                      .toString(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            throw snapshot.error;
+                          } else {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                        },
+                      ),
+                      height: screenheight / 8,
+                      width: screenwidth / 2.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.fromLTRB(0, 15, 15, 15),
+                    color: Color.fromRGBO(18, 18, 18, 1),
+                    child: Container(
+                      height: screenheight / 8,
+                      width: screenwidth / 2.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.fromLTRB(15, 15, 0, 15),
+                    color: Color.fromRGBO(18, 18, 18, 1),
+                    child: Container(
+                      height: screenheight / 8,
+                      width: screenwidth / 2.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.fromLTRB(0, 15, 15, 15),
+                    color: Color.fromRGBO(18, 18, 18, 1),
+                    child: Container(
+                      height: screenheight / 8,
+                      width: screenwidth / 2.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
-    ));
+    );
   }
 }
